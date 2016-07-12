e35a3f4dadaac0032da079d774f79ddc1dd13d4e
Nefermu <onuron@gmail.com>
2016-07-12 21:21:39 +0300

    Django uygulaması, ilk commit

---
+++ python_intro.py
@@ -0,0 +1,11 @@
+def hi(name):
+    print('Selam ' + name + '!')
+
+girls = ['Seda', 'Gül', 'Pınar', 'Ayşe', 'Sen']
+for name in girls:
+    hi(name)
+    print('Sıradaki')
+
+for i in range (1,6):
+    print (i)
+    
